94cf08a700343c150c43a3addf593f5508f35b95
Firman Taruna Nugraha <firmantr3@gmail.com>
2018-12-26 00:16:32 +0700

    rename kategoris

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,4 +1,4 @@
 <li class="{{ Request::is('kategoris*') ? 'active' : '' }}">
-    <a href="{!! route('kategoris.index') !!}"><i class="fa fa-edit"></i><span>Kategoris</span></a>
+    <a href="{!! route('kategoris.index') !!}"><i class="fa fa-edit"></i><span>Kategori</span></a>
 </li>
 
