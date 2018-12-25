239e05d605895eff4f43b10c3de3cfc65f23dd9c
Firman Taruna Nugraha <firmantr3@gmail.com>
2018-12-26 00:14:09 +0700

    update base views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>InfyOm Generator</title>
+    <title>{{ config('app.name') }}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     <!-- Bootstrap 3.3.7 -->
@@ -37,7 +37,7 @@
 
             <!-- Logo -->
             <a href="#" class="logo">
-                <b>InfyOm</b>
+                <b>{{ config('app.name') }}</b>
             </a>
 
             <!-- Header Navbar -->
@@ -100,7 +100,7 @@
 
         <!-- Main Footer -->
         <footer class="main-footer" style="max-height: 100px;text-align: center">
-            <strong>Copyright © 2016 <a href="#">Company</a>.</strong> All rights reserved.
+        <strong>Copyright © {{ date('Y') }} <a href="#">{{ config('app.name') }}</a>.</strong> All rights reserved.
         </footer>
 
     </div>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,4 @@
+<li class="{{ Request::is('kategoris*') ? 'active' : '' }}">
+    <a href="{!! route('kategoris.index') !!}"><i class="fa fa-edit"></i><span>Kategoris</span></a>
+</li>
+
